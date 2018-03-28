e12868515e03e7660063ef45683d0372852aedc1
lab2112 <brandon.coates@gmail.com>
2018-03-28 00:02:52 -0400

    removed hard coded URL

--- urlstats.py
+++ urlstats.py
@@ -1,12 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import argparse
 
-content = requests.get('http://cnn.com').text
+# Getting CLI arguments to find website to load up
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', '-u', help="The url to lookup")
+args = parser.parse_args()
+
+
+content = requests.get(args.url).text
 soup = BeautifulSoup(content, 'html.parser')
+print('***Stats for {}***'.format(args.url))
 print('There are {} anchors'.format(len(soup.find_all('a'))))
 print('There are {} link'.format(len(soup.find_all('link'))))
 print('There are {} scripts'.format(len(soup.find_all('script'))))
 
-for link in soup.find_all('a'):
+'''for link in soup.find_all('a'):
     if 'https' in link.get('href') or 'http' in link.get('href'):
-        print(link.get('href'))
+        print(link.get('href'))'''
